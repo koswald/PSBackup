83505e536540737fb708bb58030896b4f1079d27
Karl Oswald <karlfoswald@gmail.com>
2020-05-29 20:29:24 -0700

    add confirm switch

--- Setup.ps1
+++ Setup.ps1
@@ -9,6 +9,9 @@ Requires a restart of the PowerShell console for changes to take effect.
 .Parameter ProfileName
 Specifies which profile file to use. The default is CurrentUserAllHosts. Must be one of CurrentUserAllHosts, CurrentUserCurrentHost, AllUsersAllHosts, or AllUsersCurrentHost. Alias: pn.
 
+.Parameter Confirm
+If -Confirm is specified, then a prompt appears for verification before the profile file is created or changed. Default is to not confirm.
+
 .Notes
 Appends a line to the default or specified profile file to add the project path to the PSModulePath environment variable.
 #>
@@ -22,7 +25,9 @@ param(
         'AllUsersCurrentHost' )]
     [alias( 'pn' )]
     [string]
-    $ProfileName = 'CurrentUserAllHosts'
+    $ProfileName = 'CurrentUserAllHosts', 
+
+    [switch] $Confirm
 )
 # build the project Module path
 
@@ -46,23 +51,39 @@ if( $paths -contains $ProjectModulePath )
 {
     "The project's module path has already been added to the PSModulePath variable."
 }
-else {
-    # The project module path has not already been added, so ask to add it.
-
+else  # The project module path has not been added yet.
+{
     $file = $profile.$ProfileName
+    $lineToAdd = "`$env:PSModulePath += '$delimiter$ProjectModulePath'"
 
-    "Do you want to add the project path to the $ProfileName profile file ($file)?"
-
-    $key = $host.UI.RawUI.ReadKey()
-    if( 'y' -like $key.Character )
+    if( $Confirm )
     {
-        $acArgs = @{
-            Path = $file
-            Value = "`$env:PSModulePath += '$delimiter$ProjectModulePath'"
+        "Do you want to add the line `n`n$lineToAdd `n`nto the $ProfileName profile file '$file'?"
+        $key = $host.UI.RawUI.ReadKey(); "`n"
+        if( -Not ( 'y' -like $key.Character ))
+        {
+            "The profile has not been modified."
+            Exit
         }
-        Add-Content @acArgs
-
-        "The path has been added. Restart the console for changes to take effect."
     }
-    else { "`nThe new path was not added." }
+    $acArgs = @{
+        Path = $file
+        Value = $lineToAdd
+        Force = $true
+        ErrorAction = 'Stop'
+    }
+    try {
+        if( -Not ( Test-Path $file ))
+        {
+            New-Item $file -Force | Out-Null
+        }
+        Add-Content @acArgs 
+    }
+    catch {
+        $_
+        "The profile has not been modified."
+        Exit
+    }
+    "The path has been added. Restart the console for changes to take effect."
 }
+
