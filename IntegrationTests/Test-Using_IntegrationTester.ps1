8a5bfbfef3af69787b944061e5d25286037ed49d
Karl Oswald <karlfoswald@gmail.com>
2020-06-20 19:00:07 -0700

    Unused

--- IntegrationTests/Test-Using_IntegrationTester.ps1
+++
@@ -1,5 +0,0 @@
-using Module IntegrationTester
-
-"If there is an error message in red, 'Unable to find type [SomeType]', then the {bug-candidate} is present or the .psd1 file is invalid, or the test failed for another reason"
-
-$t = [IntegrationTester]::new()
